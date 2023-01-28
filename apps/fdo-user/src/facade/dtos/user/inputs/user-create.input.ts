d958bee9914c3a9651f293048cf2ebd9f9ceb38b
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-01-28 02:31:58 +0100

    chore: delete old user folder

--- apps/fdo-user/src/facade/dtos/user/inputs/user-create.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, OmitType } from '@nestjs/graphql';
-import { UserInput } from './user.input';
-
-@InputType()
-export class UserCreateInput extends OmitType(UserInput, ['id']) {}
--- apps/fdo-user/src/facade/dtos/user/inputs/user-criteria.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType, PartialType, PickType } from '@nestjs/graphql';
-import { UserInput } from './user.input';
-
-@InputType()
-export class UserCriteriaInput extends PartialType(
-  PickType(UserInput, ['id', 'email']),
-) {}

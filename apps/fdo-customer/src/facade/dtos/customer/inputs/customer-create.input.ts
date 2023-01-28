db615f2e9a28c955f339ff6ecfa072532644fd33
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-01-28 12:05:56 +0100

    chore: remove older customers service

--- apps/fdo-customer/src/facade/dtos/customer/inputs/customer-create.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, OmitType } from '@nestjs/graphql';
-import { CustomerInput } from './customer.input';
-
-@InputType()
-export class CustomerCreateInput extends OmitType(CustomerInput, ['id']) {}
--- apps/fdo-customer/src/facade/dtos/setting/inputs/setting-criteria.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType, PartialType, PickType } from '@nestjs/graphql';
-import { SettingInput } from './setting.input';
-
-@InputType()
-export class SettingCriteriaInput extends PartialType(
-  PickType(SettingInput, ['id', 'code']),
-) {}
